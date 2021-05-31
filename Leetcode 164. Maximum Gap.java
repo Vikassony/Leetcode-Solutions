80d60008a8e3dff3104eb154e423b960f9a25355
Vikassony <48961830+Vikassony@users.noreply.github.com>
2021-05-31 11:25:19 +0530

    Create Leetcode 164. Maximum Gap.java

---
+++ Leetcode 164. Maximum Gap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length==0 || nums.length==1)
+            return 0;
+        Arrays.sort(nums);
+        int res=0;
+        for(int i=0;i<nums.length-1;i++)
+            if(nums[i+1]-nums[i]>res)
+                res=nums[i+1]-nums[i];
+        return res;
+                
+    }
+}
