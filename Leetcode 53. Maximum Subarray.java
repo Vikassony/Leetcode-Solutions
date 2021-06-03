debb7b6aea50f3db8094afc846fb567f3718db1d
Vikassony <48961830+Vikassony@users.noreply.github.com>
2021-06-03 18:43:57 +0530

    Create Leetcode 53. Maximum Subarray.java

---
+++ Leetcode 53. Maximum Subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int maxSubArray(int[] nums) {
+		if (nums.length == 1)
+			return nums[0];
+		int max_so_far = Integer.MIN_VALUE;
+		int max_end_here = 0;
+		for (int i = 0; i < nums.length; i++) {
+			max_end_here = max_end_here + nums[i];
+			if (max_so_far < max_end_here)
+				max_so_far = max_end_here;
+			if (max_end_here < 0)
+				max_end_here = 0;
+		}
+		return max_so_far;
+	}
+}
