95ec69426f29b27917457501e6aabafc0b9490a9
Vikassony <48961830+Vikassony@users.noreply.github.com>
2021-06-27 20:18:27 +0530

    Update Leetcode 1913. Maximum Product Difference Between Two Pairs.java

--- Leetcode 1913. Maximum Product Difference Between Two Pairs.java
+++ Leetcode 1913. Maximum Product Difference Between Two Pairs.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int maxProductDifference(int[] nums) {
-       int max=0;
-        Arrays.sort(nums);
-        int i=0,k=nums.length-1;
-        while(i<k)
-        {
-            if((nums[k-1]*nums[k])-(nums[i]*nums[i+1])>max)
-                max=(nums[k-1]*nums[k])-(nums[i]*nums[i+1]);
-            i++;
-            k--;
-        }
-        return max;
-    }
+	public int maxProductDifference(int[] nums) {
+		int l = 0, sl = 0, s = Integer.MAX_VALUE, ss = Integer.MAX_VALUE;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] >= l) {
+				sl = l;
+				l = nums[i];
+			} else if (nums[i] > sl) {
+				sl = nums[i];
+			}
+			if (nums[i] < s) {
+				ss = s;
+				s = nums[i];
+			} else if (nums[i] < ss) {
+				ss = nums[i];
+			}
+		}
+		return (l * sl) - (s * ss);
+	}
 }
